e4ba1f2c9365bac5492162d4493998ec91fad8f1
DianVK <diankinanev@abv.bg>
2023-01-30 12:04:41 +0200

    Test Missile Generator

---
+++ __init__.py
@@ -0,0 +1,16 @@
+import time
+from Projects.randommissilegenerator.generator import generate_random_missile
+
+
+choose_option = int(input("Choose option, 1 for indefinitely or 2 for fixed number: "))
+if choose_option == 1:
+    while True:
+        missile = generate_random_missile()
+        print(f"Missile type '{missile.missile_type}' was generated!")
+        time.sleep(3)
+elif choose_option == 2:
+    option_2 = int(input("Choose number greater than 10: "))
+    for _ in range(0, option_2):
+        missile = generate_random_missile()
+        print(f"Missile type '{missile.missile_type}' was generated!")
+        time.sleep(3)
